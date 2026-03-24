c9697cd64c094b3baed99457008d5dccd7071dd2
Lakhitha Erandunu <lakhitha015@gmail.com>
2026-03-24 18:54:39 +0530

    removed the file because of an merging error

--- backend/src/main/java/com/smartcampus/backend/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smartcampus.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smartcampus.backend.model.TestEntity;
-
-public interface TestRepository extends JpaRepository<TestEntity,Long>{
-}
